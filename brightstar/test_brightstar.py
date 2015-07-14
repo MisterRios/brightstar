99dd1e118650f8074d1b010f342c7739e84da5b9
daniel rios <misterrios@gmail.com>
2015-07-14 14:37:59 +0200

    tests: added instantiation test

--- brightstar/test_brightstar.py
+++ brightstar/test_brightstar.py
@@ -15,10 +15,24 @@ TEST_CONFIG = { 'datacentre': 'eu1',
 class InstantiationTest(unittest.TestCase):
 
     def setUp(self):
-        test_instance = API(TEST_CONFIG)
+        self.instance = API(TEST_CONFIG)
 
     def test_successful_instance(self):
-        pass
+        
+        self.assertEqual(self.instance.datacentre, 'eu1')
+        self.assertEqual(self.instance.api_version, '2.0.0')
+        self.assertEqual(self.instance.account_code, 'testcompany')
+        self.assertEqual(self.instance.app_ref, 'testcompany_testapp')
+        self.assertEqual(self.instance.authentication_token, 
+            'f4dtgpjl89z0aftgpj89z0a')
+        self.assertIsNone(self.instance.staff_authentication_token)
+
+        self.assertEqual(
+            self.instance.headers, {
+            "brightpearl-app-ref": 'testcompany_testapp',
+            "brightpearl-account-token": 'f4dtgpjl89z0aftgpj89z0a'
+            }
+            )
 
 
 
